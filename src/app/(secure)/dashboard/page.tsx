5d41ba5fcaf4a5e1ae29c8106dbaaf450a2cd471
jradness <jabrewer818@gmail.com>
2024-02-18 20:54:12 -0600

    removed auth routes

--- src/app/(secure)/dashboard/page.tsx
+++ src/app/(secure)/dashboard/page.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import DashboardTabs from '../../_components/DashboardTabs/DashboardTabs';
 import {Container} from "react-bootstrap";
-import { withPageAuthRequired, getSession } from '@auth0/nextjs-auth0';
+// import { withPageAuthRequired, getSession } from '@auth0/nextjs-auth0';
 
 const Dashboard = async () => {
-  const session = await getSession();
+  // const session = await getSession();
  return (
     <Container>
-      {session && session?.user && (
+      {/* {session && session?.user && (
         <div>{session.user.email} - <a href="/api/auth/logout">Logout</a></div>
-      )}
+      )} */}
       <h1>Money Budgeting App</h1>
       {/* <DashboardTabs /> */}
       </Container>
   );
 };
 
-export default withPageAuthRequired(Dashboard, { returnTo: '/' });
+export default Dashboard;
+// export default withPageAuthRequired(Dashboard, { returnTo: '/' });
