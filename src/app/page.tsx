6599571af7b0cc77201f81bb1f5f723be8efe646
jradness <jabrewer818@gmail.com>
2024-03-20 10:02:05 -0500

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,19 +11,25 @@ export default function Home() {
         <div className="wave"></div>
         <div className="wave"></div>
         <h1>Welcome to Budget Radness 💰</h1>
-        <p className={s.appDescription}>Overview: This app helps give you better insights into “approximately” what bills fall into a given pay-period and what your leftover spending is for a <b>bi-weekly pay schedule</b>.
+        <p className={s.appDescription}>
+          Discover enhanced clarity in managing your finances with our app.
+          Designed to provide a clearer view of your bi-weekly budget, it estimates which bills align with each pay period and calculates your available spending money.
         <br />
         <br />
-        <b>NOTE:</b> Calculations are made on “fixed” bill due dates that you provide. Needless to say (though I&apos;m gonna say it 😉), in real-life, actual bill due dates vary depending on weekends, holidays, etc.</p>
+        <b>Important to Note: </b>
+        The app operates on the bill due dates you input.
+        Remember, real-life scenarios might differ as actual bill due dates can shift due to weekends, holidays, and other variables.
+        We've got you covered with these estimations, but always stay attuned to the actual dates.
+        </p>
         <h3 className={s.featureTitle}>Features:</h3>
         <ul className={s.featureList}>
-          <li>Securely create and login to your account via <a style={{textDecoration: "underline"}}href="https://auth0.com/">Auth0</a></li>
-          <li>Database enforces TLS/SSL (Transport Layer Security/Secure Sockets Layer) to encrypt all network traffic. TLS/SSL ensures that database network traffic is only readable by the intended client.</li>
-          <li>Forecast bi-weekly pay. Know exactly what bills fall within those 2 weeks, bill total, and left overspending for those two weeks.</li>
-          <li>Add/update financial details to calculate forecasted pay periods.</li>
-          <li>Add/update/delete recurring expense for the pay period</li>
-          <li>Safely and securely delete your account</li>
-          <li>Support for weekly, bi-monthly, monthly pay schedules coming soon.</li>
+        <li>Secure account creation and login using <a style={{textDecoration: "underline"}} href="https://auth0.com/">Auth0</a> for enhanced security.</li>
+        <li>Enhanced data protection with TLS/SSL encryption, safeguarding all network traffic for your peace of mind.</li>
+        <li>Accurately forecast your bi-weekly pay, including detailed insights into bills due, total bill amount, and discretionary spending for each pay period.</li>
+        <li>Easily manage your finances by adding or updating your financial details for pay period forecasting.</li>
+        <li>Conveniently add, modify, or remove recurring expenses to align with your pay schedule.</li>
+        <li>Secure and straightforward account deletion process, ensuring your privacy and peace of mind.</li>
+        <li>Coming soon: Support for additional pay schedules including weekly, bi-monthly, and monthly.</li>
         </ul>
         <small>(v1.0.1 Beta 1)</small>
         <Button className={s.subText} href={returnLink}>Login / Sign Up</Button>
